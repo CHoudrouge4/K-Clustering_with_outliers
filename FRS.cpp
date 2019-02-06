d32e70b9c03ddb370b6a083b664d4aaa89cf803a
CHoudrouge4 <hah51@mail.aub.edu>
2019-02-06 17:23:15 +0100

    FRS updates

--- FRS.cpp
+++ FRS.cpp
@@ -2,12 +2,20 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 FRS::FRS(std::string file_name, double r) {
 	read_points(file_name);
 	std::cout << min_x << ' ' << max_x << '\n';
 	std::cout << min_y << ' ' << max_y << '\n';
+	radius = r;
+	radius_2 = r * r;
+}
 
+double FRS::distance_2(const int p, const int q) const {
+	double dx = points[p][0] - points[q][0];
+	double dy = points[p][1] - points[q][1];
+	return dx * dx + dy * dy;
 }
 
 void FRS::read_points(const std::string file_name) {
@@ -32,5 +40,42 @@ void FRS::read_points(const std::string file_name) {
 	std::cout << "Finish Reading Points" << '\n';
 }
 
+int FRS::get_grid_dimension() const {
+	double max_dis_x = std::abs(max_x - min_x);
+	double max_dis_y = std::abs(max_y - min_y);
+	double dim = std::max(max_dis_x, max_dis_y);
+	return std::ceil(dim)/radius;
+}
+
+int FRS::get_i_index(const size_t p) const {
+	double px = points[p][0];
+	return  std::floor(std::abs(px - min_x)) / radius;
+}
+
+int FRS::get_j_index(const size_t p) const {
+	double py = points[p][1];
+	return std::floor(std::abs(py - min_y))/ radius;
+}
+
 void FRS::build_2d_grid() {
+//	get_grid_dimension();
+	for(size_t i = 0; i < points.size(); ++i) {
+		std::pair<int, int> p = {get_i_index(i), get_j_index(i)};
+		grid[p].push_back(i);
+	}
+}
+
+vec_int FRS::querry_disk_r(const size_t q) {
+	int qi = get_i_index(q);
+	int qj = get_j_index(q);
+	vec_int res;
+	for(int i = qi - 1; i < qi + 2; i++) {
+		for(int j = qj - 1; j < qj + 2; j++) {
+		//	if(distance_2())
+			std::pair<int, int> p = {i, j};
+			auto v = grid[p];
+			res.insert(res.end(), v.begin(), v.end());
+		}
+	}
+	return res;
 }
--- FRS.h
+++ FRS.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <limits>
+#include <map>
 
 typedef std::vector<double> point;
 typedef std::vector<int> vec_int;
@@ -15,19 +16,25 @@ private:
 	double min_y = min_x;
 	double max_y = max_x;
 
-	double raduis;
+	double radius;
+	double radius_2;
 
 	vec_pts points;
-	std::vector<vec_int> grid;
+	std::map<std::pair<int, int>, vec_int> grid;
+
 
 	void read_points(const std::string file_name);
 	void build_2d_grid();
-	FRS() {};
+	int get_grid_dimension() const;
+	int get_i_index(const size_t p) const;
+	int get_j_index(const size_t p) const;
+
+	double distance_2(const int p, const int q) const;
 
 public:
 	FRS(std::string file_name, double r);
 
-	std::vector<int> querry_points(const point &q);
+	vec_int querry_disk_r(const size_t q);
 	point get_point(const size_t) const;
 	vec_pts get_points(const vec_int & indices);
 	void new_raduis(const double&);
